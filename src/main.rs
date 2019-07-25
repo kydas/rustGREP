ed08d35ca6093f2dcd1c1d1bd916eb50806a8fc9
dasilvar93 <kadasilva22@gmail.com>
2019-07-25 09:03:19 -0700

    Reads from file now!

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -7,4 +8,9 @@ fn main() {
     let filename = &args[2];
     println!("Searching for {}", query);
     println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
 }
